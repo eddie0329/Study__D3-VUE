0def28f5e3293946635b480f60b7eb853679c30c
eddie <fdsa0329@naver.com>
2020-05-08 23:17:47 +0900

    Add wunderman-commerce practice

---
+++ wunderman-commerce-practice/src/components/MyChart.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <svg xmlns="http://www.w3.org/2000/svg" width="500" height="400">
+      <g v-for="(value, i) in chartData" :key="value" :transform="`translate(0, ${i * 40}`">
+        <rect width="100" height="30" fill="#f0b375" :y="i * 40" />
+        <text :y="i * 39">{{ value }}</text>
+      </g>
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      chartData: [5, 8, 20, 3, 17, 14],
+      width: 100,
+      height: 30,
+      x: 10,
+      y: 10
+    };
+  }
+};
+</script>
+
+<style>
+</style>
